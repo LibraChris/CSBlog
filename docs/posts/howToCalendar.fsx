57cdd96f59c5d50e9ee5572e6e21951a56b7e9fe
Libra_Chris <59606965+LibraChris@users.noreply.github.com>
2021-11-29 15:02:52 +0100

    Create howToCalendar.fsx

---
+++ docs/posts/howToCalendar.fsx
@@ -0,0 +1,43 @@
+(**
+
+---
+title: How to Calendar
+category: Tools
+categoryindex: 5
+index: 0
+---
+
+# An introduction to our self-hosted calendar on HyperV for server booking
+
+## Content
+- [The joys of a working booking system](#The-joys-of-a-working-booking-system)
+- [The not self-hosted beginnings](#The-not-self-hosted-beginnings)
+- [Why self-hosting a calendar may be a good idea](#Why-self-hosting-a-calendar-may-be-a-good-idea)
+- [CalDAV](#CalDAV)
+- [Our current system](#Our-current-system)
+- [Step by step guide](#Step-by-step-guide)
+- [Github Actions](#Github-Actions)
+- [Self-hosted alternatives](#Self-hosted-alternatives)
+- [Further reading](#Further-reading)
+
+## The joys of a working booking system
+## The not self-hosted beginnings
+## Why self-hosting a calendar may be a good idea
+## CalDAV
+## Our current system
+## Step by step guide
+## Github Actions
+## Self-hosted alternatives
+### Server
+- [Radicale](https://radicale.org/3.0.html)
+- [Baïkal](https://sabre.io/baikal/)
+
+### Interface
+- [InfCloud](https://www.inf-it.com/open-source/clients/infcloud/)
+
+## Further reading
+
+- [Reddit source for self-hosted services](https://www.reddit.com/r/selfhosted/)
+- [Github repo for self-hosted servives](https://github.com/awesome-selfhosted/awesome-selfhosted)
+
+*)
